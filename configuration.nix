691843055add4aa667a6908a257332a6579da98c
OliveIsAWord <89336634+OliveIsAWord@users.noreply.github.com>
2024-06-22 11:11:27 -0600

    brightnessctl

--- configuration.nix
+++ configuration.nix
@@ -91,16 +91,16 @@
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
-
   # Enable sound with pipewire.
   sound.enable = true;
+  hardware.pulseaudio.enable = false;
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    jack.enable = true;
+    # jack.enable = true;
   };
 
   users.users.olive = {
@@ -129,6 +129,7 @@
       prismlauncher
       zoxide
       hyfetch
+      brightnessctl
       wrappers.all
     ];
   };
