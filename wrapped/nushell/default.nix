9068bebbfac2b2525a7cd5381269962ef0caa223
OliveIsAWord <89336634+OliveIsAWord@users.noreply.github.com>
2024-12-04 22:04:38 -0700

    starship and carapace

--- wrapped/nushell/default.nix
+++ wrapped/nushell/default.nix
@@ -2,12 +2,16 @@
   pkgs,
   lib,
   ...
-}: let
-  configSource = lib.strings.concatStrings (map builtins.readFile [./config.nu ./starship.nu ./zoxide.nu]);
-  configFile = pkgs.writeText "config.nu" configSource;
-in {
+}: {
   wrappers.nushell = {
     basePackage = pkgs.nushell;
-    flags = ["--config" "${configFile}" "--env-config" ./env.nu];
+    flags = ["--config" ./config.nu "--env-config" ./env.nu];
+    env.STARSHIP_CONFIX = {
+    force = true;
+    value = ./starship.toml; };
+    pathAdd = [
+        pkgs.starship
+	pkgs.carapace
+    ];
   };
 }
